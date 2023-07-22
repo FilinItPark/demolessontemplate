c5144d587afdc3da648d8aef0bd25a049d961ff5
IVBerezutskij <ivberezutskij@mai.education>
2023-07-22 10:58:51 +0300

    first commit

---
+++ src/main/java/org/example/GuessNumberBot.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class GuessNumberBot extends TelegramLongPollingBot {
+
+    String botUsername;
+    String botToken;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
