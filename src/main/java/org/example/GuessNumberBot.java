5691b6e342f950a22fa3d2109904b8f26d8d2695
Ivan <54151565+1ommyS@users.noreply.github.com>
2023-10-01 18:50:47 +0300

    ewew

--- src/main/java/org/example/GuessNumberBot.java
+++ src/main/java/org/example/GuessNumberBot.java
@@ -10,52 +10,15 @@ import java.util.Random;
 
 public class GuessNumberBot extends TelegramLongPollingBot {
 
-    String botUsername = "ibdemolessonbot";
-    String botToken = "6434891171:AAEBgOFhmxgZ7_w73v3i3h-A32EVZiGhS18";
+    String botUsername = "";
+    String botToken = "";
     Random random = new Random();
     int computerNumber;
     int userNumber;
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() == true) {
-            Message message = update.getMessage();
 
-            if (message.hasText() == true) {
-                String textFromMessage = message.getText();
-                long chatId = message.getChatId();
-
-                SendMessage messageToUser = new SendMessage();
-
-                messageToUser.setChatId(chatId);
-
-                if (textFromMessage.equals("/start") == true) {
-                    computerNumber = random.nextInt(1, 101);
-                    messageToUser.setText("Привет! Я бот угадай-число, угадай число от 1 до 100");
-
-                } else {
-                    userNumber = Integer.parseInt(textFromMessage);
-
-                    if (userNumber == computerNumber) {
-                        messageToUser.setText("Ура! Вы умница, вы победили");
-                    }
-                    if (userNumber < computerNumber) {
-                        messageToUser.setText("Введите число побольше");
-                    }
-                    if (userNumber > computerNumber) {
-                        messageToUser.setText("Введите число поменьше");
-                    }
-                }
-
-
-
-                try {
-                    execute(messageToUser);
-                } catch (TelegramApiException e) {
-
-                }
-            }
-        }
     }
 
 
